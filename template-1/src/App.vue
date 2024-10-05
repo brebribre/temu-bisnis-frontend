262ba2a2144bc1af6a6b9f87262c66dc6b444225
brebribre <70563428+brebribre@users.noreply.github.com>
2024-10-05 12:48:09 +0200

    project setup

---
+++ template-1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+    <p class="mt-4 text-lg text-gray-700">This is a test for Tailwind CSS.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ template-1/src/App.vue.js
@@ -0,0 +1,55 @@
+const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
+const __VLS_fnComponent = (await import('vue')).defineComponent({});
+;
+let __VLS_functionalComponentProps;
+function __VLS_template() {
+    const __VLS_ctx = {};
+    const __VLS_localComponents = {
+        ...{},
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_components;
+    const __VLS_localDirectives = {
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_directives;
+    let __VLS_styleScopedClasses;
+    __VLS_styleScopedClasses['logo'];
+    __VLS_styleScopedClasses['logo'];
+    // CSS variable injection 
+    // CSS variable injection end 
+    let __VLS_resolvedLocalAndGlobalComponents;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({ ...{ class: ("text-center") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.h1, __VLS_intrinsicElements.h1)({ ...{ class: ("text-4xl font-bold text-blue-500") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({ ...{ class: ("mt-4 text-lg text-gray-700") }, });
+    __VLS_styleScopedClasses['text-center'];
+    __VLS_styleScopedClasses['text-4xl'];
+    __VLS_styleScopedClasses['font-bold'];
+    __VLS_styleScopedClasses['text-blue-500'];
+    __VLS_styleScopedClasses['mt-4'];
+    __VLS_styleScopedClasses['text-lg'];
+    __VLS_styleScopedClasses['text-gray-700'];
+    var __VLS_slots;
+    var __VLS_inheritedAttrs;
+    const __VLS_refs = {};
+    var $refs;
+    return {
+        slots: __VLS_slots,
+        refs: $refs,
+        attrs: {},
+    };
+}
+;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+;
